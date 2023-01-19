abd6e044f847e0ed955ba0db4b01e33daf19ba84
Menchumenx <menchumenx@gmail.com>
2023-01-19 17:59:45 +0100

    dia1 COMPONENTS: header, footer, register, form-register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Host, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ResgisterComponent } from './pages/resgister/resgister.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent },
+  {path: 'registro', component:ResgisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { ResgisterComponent } from './pages/resgister/resgister.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormRegisterComponent,
+    ResgisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
